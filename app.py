2710caf4c6ce4748a167418c375647320ee455da
ashwini kakde <zileashwini12@gmail.com>
2023-06-23 14:43:43 +0530

    new file main.yaml

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def predict_data():
 if __name__ == '__main__': 
     try:
         #app.run(host='0.0.0.0',port=5002,debug=True)
-        app.run(host='0.0.0.0')
+        app.run(host='0.0.0.0',port=5000,debug=True)
     except Exception as e:
         raise ThyroidException(e,sys)
